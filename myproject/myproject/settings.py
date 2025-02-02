d3ee470764d0a2fb7a46e6dda66d6c7bbf66ec2a
phanna <soksiphanna35@gmail.com>
2025-02-02 13:53:42 +0700

    on the way

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Default primary key field tywpe
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
